e2232bf3b8e3559f4e2add317d02a6f6bd251154
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-02-16 13:22:11 +0100

    refactored: using map to call our component only once, and abstracting another function from our component, resulting in our component having 2 props instead of 1

--- src/AlertButton.tsx
+++ src/AlertButton.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 
-type ButtonComponents = {
+type ButtonProps = {
     alert: string
+    clickHandler: ()=> void   //means its type is 'function'
 }
 
-
-function AlertButton ({alert}: ButtonComponents) {
+function AlertButton (props: ButtonProps) {
     return (
         <button 
-            className = {alert} >
-            {/* // onClick = {() => window.alert("That was" + " " + alert)} >  */}
-        {alert} 
+            className = {props.alert} 
+            onClick = {props.clickHandler} >
+        {props.alert} 
         </button>
     )
 }
 
 export default AlertButton
+
+
+// Exercise 2b: Passing a function into the event listener
+// Adapt your existing solution in order to define a function which takes a single parameter and creates an alert box saying that was [parameter]. Now pass this function to each button.
+
--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,23 @@ import React from 'react';
 import './App.css';
 import AlertButton from './AlertButton'
 
+const buttons = ['easy', 'okay', 'difficult', 'too_difficult', 'medium', 'easy']
+
+
 
 function App() {
-
+  
   return (
     <div className= "App">
-      <AlertButton alert = 'easy'  />
-      <AlertButton alert = 'okay' />
-      <AlertButton alert = 'difficult'  />
-      <AlertButton alert = 'too_difficult'  />
-      
-
-      {/* <button 
-        className="button1" 
-        onClick={() => alert("That was easy")}
-        > easy </button>
-      <button 
-        className="button2" 
-        onClick={() => alert("That was okay")}
-        > okay </button>
-      <button 
-        className="button3" 
-        onClick={() => alert("That was difficult")}
-        > difficult </button>
-      <button 
-        className="button4" 
-        onClick={() => alert("That was too difficult")}
-        > too difficult </button> */}
+      {buttons.map(element => (
+        <AlertButton 
+          alert = {element} 
+          clickHandler = {() => window.alert("That was " + element)} /> 
+      ))}
     </div>
   );
 }
+
+
 
 export default App;
